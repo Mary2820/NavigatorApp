2afa7b3d062def44061894f97cd171a00d411bfc
PawelR9 <pawelrylski@protonmail.com>
2025-02-26 21:04:06 +0100

    Removed unnecessary methods.

--- src/main/java/com/solvd/navigationapp/DAO/ICityDAO.java
+++ src/main/java/com/solvd/navigationapp/DAO/ICityDAO.java
@@ -4,16 +4,10 @@ import com.solvd.navigationapp.models.City;
 
 import java.util.List;
 
-public interface ICityDAO extends IGenericDAO<City, Long> {
+public interface ICityDAO extends IDAO<City> {
     List<City> getByName(String name);
 
     List<City> getByRegion(String region);
 
     List<City> getByCountry(String country);
-
-    List<City> getByCountryAndRegion(String country, String region);
-
-    int countCitiesInRegion(String region);
-
-    int countCitiesInCountry(String country);
 }
