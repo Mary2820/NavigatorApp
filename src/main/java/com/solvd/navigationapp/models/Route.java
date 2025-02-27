2be09b36a76bb8fd20d63c787ac9f7a8ce898570
DESKTOP-A26UO4C\mdedu <mdedul2820@gmail.com>
2025-02-27 09:45:41 +0100

    Created: Graph.

--- src/main/java/com/solvd/navigationapp/models/Route.java
+++ src/main/java/com/solvd/navigationapp/models/Route.java
@@ -1,20 +1,26 @@
 package com.solvd.navigationapp.models;
 
+import java.util.Objects;
+
 public class Route {
+    private static final int MASK = 1;
     private Long id;
     private Long startPointId;
     private Long endPointId;
     private Long vehicleId;
+    private Integer distance;
+    private boolean isBidirectional;
 
-    public Route(Long id, Long startPointId, Long endPointId, Long vehicleId) {
+    public Route(Long id, Long startPointId, Long endPointId, Long vehicleId, Integer distance) {
         this.id = id;
         this.startPointId = startPointId;
         this.endPointId = endPointId;
         this.vehicleId = vehicleId;
+        this.distance = distance;
+        this.isBidirectional = true;
     }
 
-    public Route() {
-    }
+    public Route() { }
 
     public Long getId() {
         return id;
@@ -46,5 +52,37 @@ public class Route {
 
     public void setVehicleId(Long vehicleId) {
         this.vehicleId = vehicleId;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+
+    public boolean isBidirectional() {
+        return isBidirectional;
+    }
+
+    public void setBidirectional(boolean bidirectional) {
+        isBidirectional = bidirectional;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (hashCode() != o.hashCode()) return false;
+        Route route = (Route) o;
+        return Objects.equals(id, route.id) && Objects.equals(startPointId, route.startPointId) &&
+                Objects.equals(endPointId, route.endPointId) && Objects.equals(vehicleId, route.vehicleId) &&
+                Objects.equals(distance, route.distance) && isBidirectional == route.isBidirectional;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, startPointId, endPointId, vehicleId, distance, isBidirectional);
     }
 }
