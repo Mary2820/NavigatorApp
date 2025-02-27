cbc386cb5d2d2997bea63b0fa075c3bc02c30be6
DESKTOP-A26UO4C\mdedu <mdedul2820@gmail.com>
2025-02-27 23:10:52 +0100

    Added: annotations for Jaxb and Jackson.

--- src/main/java/com/solvd/navigationapp/models/Driver.java
+++ src/main/java/com/solvd/navigationapp/models/Driver.java
@@ -1,30 +1,31 @@
 package com.solvd.navigationapp.models;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import com.solvd.navigationapp.enums.UserType;
 
+@XmlRootElement(name = "driver")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Driver extends User {
-    private Long id;
+    
+    @XmlElement(name = "licenseId")
     private Long licenseId;
+    
+    @XmlElement(name = "phoneNumber")
+    private String phoneNumber;
 
-    public Driver(Long id, Long personId, String firstName, String lastName, Long licenseId) {
-        super(personId, firstName, lastName);
-        this.id = id;
+    public Driver() {
+        super();
+        this.userType = UserType.DRIVER;
+    }
+
+    public Driver(Long id, String firstName, String lastName, String phoneNumber, Long licenseId) {
+        super(id, firstName, lastName);
+        this.phoneNumber = phoneNumber;
         this.licenseId = licenseId;
         this.userType = UserType.DRIVER;
-    
-    }
-
-    public Driver(Long id, String firstName, String lastName) {
-        super(id, firstName, lastName);
-        this.userType = UserType.DRIVER;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public Long getLicenseId() {
@@ -35,4 +36,11 @@ public class Driver extends User {
         this.licenseId = licenseId;
     }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
