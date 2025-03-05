fce5ca8e00f688aae8afd06e7f513d41e0e6e406
DESKTOP-A26UO4C\mdedu <mdedul2820@gmail.com>
2025-03-05 14:40:45 +0100

    Added: ResultService.

---
+++ src/main/java/com/solvd/navigationapp/models/Result.java
@@ -0,0 +1,82 @@
+package com.solvd.navigationapp.models;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+import jakarta.xml.bind.annotation.*;
+
+@JsonRootName(value = "step")
+@XmlRootElement(name = "step")
+@XmlAccessorType(XmlAccessType.NONE)
+public class Result {
+    private Location startPoint;
+    private Location endPoint;
+    private Integer distance;
+    private Integer timeInMinutes;
+    private String vehicleType;
+    private String vehicleNumber;
+
+    public Result(Location startPoint, Location endPoint, Integer distance, Integer timeInMinutes, String vehicleType, String vehicleNumber) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+        this.distance = distance;
+        this.timeInMinutes = timeInMinutes;
+        this.vehicleType = vehicleType;
+        this.vehicleNumber = vehicleNumber;
+    }
+
+    public Result() {
+    }
+
+    @XmlElement(name = "startPoint")
+    public Location getStartPoint() {
+        return startPoint;
+    }
+
+    public void setStartPoint(Location startPoint) {
+        this.startPoint = startPoint;
+    }
+
+    @XmlElement(name = "endPoint")
+    public Location getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(Location endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    @XmlElement(name = "distance")
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+
+    @XmlElement(name = "timeInMinutes")
+    public Integer getTimeInMinutes() {
+        return timeInMinutes;
+    }
+
+    public void setTimeInMinutes(Integer timeInMinutes) {
+        this.timeInMinutes = timeInMinutes;
+    }
+
+    @XmlElement(name = "vehicleType")
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    @XmlElement(name = "vehicleNumber")
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public void setVehicleNumber(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+}
--- src/main/java/com/solvd/navigationapp/services/impl/TransportService.java
+++ src/main/java/com/solvd/navigationapp/services/impl/TransportService.java
@@ -3,17 +3,20 @@ package com.solvd.navigationapp.services.impl;
 import java.util.List;
 
 import com.solvd.navigationapp.models.Location;
+import com.solvd.navigationapp.models.Route;
 import com.solvd.navigationapp.services.ITransportService;
+import com.solvd.navigationapp.services.dbservices.IRouteService;
+import com.solvd.navigationapp.services.dbservices.impl.RouteService;
 
 public class TransportService implements ITransportService{
+    private final IRouteService routeService;
 
     public TransportService() {
+        this.routeService = new RouteService();
     }
 
     @Override
-    public List<Location> getTransportPath(List<Location> path) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTransportPath'");
+    public List<Route> getTransportPath(List<Location> path) {
+        return routeService.getByEndPointId(99L);
     }
-
 }
