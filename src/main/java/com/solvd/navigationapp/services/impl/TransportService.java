f85ef39398a4605515461079fce0afee8d466f75
Smoke2899 <75633084+Smoke2899@users.noreply.github.com>
2025-03-06 12:48:04 +0100

    Added: Finally added TransportService 2.

--- src/main/java/com/solvd/navigationapp/services/impl/TransportService.java
+++ src/main/java/com/solvd/navigationapp/services/impl/TransportService.java
@@ -1,15 +1,17 @@
 package com.solvd.navigationapp.services.impl;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
+import com.solvd.navigationapp.enums.VehicleType;
 import com.solvd.navigationapp.models.Location;
 import com.solvd.navigationapp.models.Route;
 import com.solvd.navigationapp.services.ITransportService;
-import com.solvd.navigationapp.services.dbservices.IRouteService;
 import com.solvd.navigationapp.services.dbservices.impl.RouteService;
 
 public class TransportService implements ITransportService{
-    private final IRouteService routeService;
+    private RouteService routeService;
 
     public TransportService() {
         this.routeService = new RouteService();
@@ -17,6 +19,29 @@ public class TransportService implements ITransportService{
 
     @Override
     public List<Route> getTransportPath(List<Location> path) {
-        return routeService.getByEndPointId(99L);
+        List<Route> transportPath = new ArrayList<>();
+        for (int i = 0; i < path.size() - 1; i++) {
+            Location currentLocation = path.get(i);
+            Location nextLocation = path.get(i + 1);
+            Route bestRoute = findBestRouteForType(currentLocation, nextLocation);
+
+            if (bestRoute != null) {
+                transportPath.add(bestRoute);
+            }
+        }
+
+        return transportPath;
+    }
+    private Route findBestRouteForType(Location startLocation, Location endLocation) {
+        return getRoutesBetweenLocations(startLocation.getId(), endLocation.getId())
+                .stream()
+                .min(Comparator.comparingDouble(route ->
+                        route.getDistance() / VehicleType.values()[(int) (long) route.getVehicleId() - 1].getSpeed()
+                ))
+                .orElse(null);
+    }
+    private List<Route> getRoutesBetweenLocations(Long startPointId, Long endPointId) {
+        return routeService.getByStartAndEndPoints(startPointId, endPointId);
     }
 }
+
